0e6329e206f72d7cea0bd1a9fc6fec65fee61380
jyostna1 <jyosthna.c@gmail.com>
2022-06-06 07:23:07 +0000

    browser router practice-22

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="home-logo"
+    />
+    <h1 className="head">About</h1>
+    <p className="description">I Love to create! I am a front End Developer</p>
+  </div>
+)
+
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="about"
+      className="home-logo"
+    />
+    <h1 className="head">Not Found</h1>
+  </div>
+)
+
+export default About
